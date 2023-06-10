697a18a6477a80e96f6a5ed88de2d49dddfdd76d
Isaac Berrio <berriisaax18@hotmail.com>
2023-06-10 10:16:47 -0500

    Commit 4

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // ...
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        // ...
+      },
+    },
+  },
 })
